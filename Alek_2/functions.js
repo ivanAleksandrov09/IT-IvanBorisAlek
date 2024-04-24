7a5d359abf356b11d233f580669d199215c23fe1
AlekPetrov <alekpetrov2009@icloud.com>
2024-04-24 21:34:45 +0300

    improved css and added scroll to bottom on click of function on jet page

--- Alek_2/functions.js
+++ Alek_2/functions.js
@@ -3,7 +3,7 @@ let fighterJets = [
     {name: "F-22 Raptor", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Lockheed_Martin_F-22A_Raptor_JSOH.jpg/1024px-Lockheed_Martin_F-22A_Raptor_JSOH.jpg", wikiUrl: "https://en.wikipedia.org/wiki/Lockheed_Martin_F-22_Raptor" },
     {name: "F-35 Lightning II", imageUrl: "https://www.lockheedmartin.com/content/dam/lockheed-martin/general/template/images/ryan169.jpg", wikiUrl: "https://en.wikipedia.org/wiki/Lockheed_Martin_F-35_Lightning_II" },
     {name: "Su-27 Flanker", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Sukhoi_Su-27SKM_at_MAKS-2005_airshow.jpg/1200px-Sukhoi_Su-27SKM_at_MAKS-2005_airshow.jpg", wikiUrl: "https://en.wikipedia.org/wiki/Sukhoi_Su-27" },
-    {name: "MiG-29 Fulcrum", imageUrl: "https://nationalinterest.org/sites/default/files/main_images/Aviamix2015-03_%28cropped%29%20%283%29%20%281%29.jpg", wikiUrl: "https://en.wikipedia.org/wiki/Mikoyan_MiG-29" },
+    {name: "MiG-29 Fulcrum", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Russian_Air_Force%2C_747%2C_Mikoyan-Gurevich_MiG-29M2.jpg", wikiUrl: "https://en.wikipedia.org/wiki/Mikoyan_MiG-29" },
 ];
 
 function showRandomFighterJet() {
@@ -13,6 +13,10 @@ function showRandomFighterJet() {
     document.getElementById("fighterJetImage").src = randomJet.imageUrl;
     document.getElementById("fighterJetLink").href = randomJet.wikiUrl;
     document.getElementById("RandomJet").style.display = "block";
+
+    //function for scroll to jet image from stackoverflow
+    let buttonPosition = document.getElementById("JetButton").getBoundingClientRect().bottom;
+    window.scrollTo({ top: buttonPosition, behavior: "smooth" });
 }
 
 document.getElementById("randomButton").addEventListener("click", showRandomFighterJet);
