7df0d3f9f0ad71ad79c930fa1fe8a65ba7263384
Ivan Alexandrov <ivan.d.aleksandrov.2023@elsys-bg.org>
2024-04-24 18:07:06 +0300

    Made another calculator and changed a "div" tag to "header" in every page

--- Ivan_2/functions.js
+++ Ivan_2/functions.js
@@ -1,5 +1,6 @@
 
 const pressure_button = document.getElementById("pressure-button");
+const time_button = document.getElementById("time-button");
 
 
 pressure_button.addEventListener("click", () => {
@@ -15,4 +16,31 @@ pressure_button.addEventListener("click", () => {
         result = (depth / 10) + 1;
     }
     pressure_result.innerHTML = `Result: ${result.toFixed(1)} ${unit}.`;
+});
+
+time_button.addEventListener("click", () => {
+    const time_result = document.getElementById("time-result");
+    
+    let unit = document.getElementById("time-unit").value;
+    let depth = document.getElementById("time-depth").value;
+    let speed = document.getElementById("time-speed").value;
+    let result = (depth * 0.001) / speed;
+    
+    if (unit === "minutes") {
+        result *= 60;   
+    } else if (unit === "seconds") {
+        result *= 3600;
+    }
+
+
+    if (result === 1) {
+        unit = unit.slice(0, -1); // Removes the 's' from the unit
+    }
+    if (result % 1 !== 0) {
+        time_result.innerHTML = `Result: ${result.toFixed(2)} ${unit}.`;
+    } else {
+        time_result.innerHTML = `Result: ${result} ${unit}.`;
+    }
+    
+    
 });
