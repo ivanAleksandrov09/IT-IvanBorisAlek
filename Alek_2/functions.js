b533ab14d44ccbe38702d7c74c8d39d856f0aa3b
AlekPetrov <alekpetrov2009@icloud.com>
2024-04-24 10:04:13 +0300

    added wikipedia urls on click of images of the random jet

--- Alek_2/functions.js
+++ Alek_2/functions.js
@@ -1,9 +1,9 @@
 let fighterJets = [
-   {name: "F-16 Fighting Falcon", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/F-16_June_2008.jpg/1200px-F-16_June_2008.jpg"},
-   {name: "F-22 Raptor", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Lockheed_Martin_F-22A_Raptor_JSOH.jpg/1024px-Lockheed_Martin_F-22A_Raptor_JSOH.jpg"},
-   {name: "F-35 Lightning II", imageUrl: "https://www.lockheedmartin.com/content/dam/lockheed-martin/general/template/images/ryan169.jpg"},
-   {name: "Su-27 Flanker", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Sukhoi_Su-27SKM_at_MAKS-2005_airshow.jpg/1200px-Sukhoi_Su-27SKM_at_MAKS-2005_airshow.jpg"},
-   {name: "MiG-29 Fulcrum", imageUrl: "https://nationalinterest.org/sites/default/files/main_images/Aviamix2015-03_%28cropped%29%20%283%29%20%281%29.jpg"},
+    {name: "F-16 Fighting Falcon", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/F-16_June_2008.jpg/1200px-F-16_June_2008.jpg", wikiUrl: "https://en.wikipedia.org/wiki/F-16_Fighting_Falcon" },
+    {name: "F-22 Raptor", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Lockheed_Martin_F-22A_Raptor_JSOH.jpg/1024px-Lockheed_Martin_F-22A_Raptor_JSOH.jpg", wikiUrl: "https://en.wikipedia.org/wiki/Lockheed_Martin_F-22_Raptor" },
+    {name: "F-35 Lightning II", imageUrl: "https://www.lockheedmartin.com/content/dam/lockheed-martin/general/template/images/ryan169.jpg", wikiUrl: "https://en.wikipedia.org/wiki/Lockheed_Martin_F-35_Lightning_II" },
+    {name: "Su-27 Flanker", imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Sukhoi_Su-27SKM_at_MAKS-2005_airshow.jpg/1200px-Sukhoi_Su-27SKM_at_MAKS-2005_airshow.jpg", wikiUrl: "https://en.wikipedia.org/wiki/Sukhoi_Su-27" },
+    {name: "MiG-29 Fulcrum", imageUrl: "https://nationalinterest.org/sites/default/files/main_images/Aviamix2015-03_%28cropped%29%20%283%29%20%281%29.jpg", wikiUrl: "https://en.wikipedia.org/wiki/Mikoyan_MiG-29" },
 ];
 
 function showRandomFighterJet() {
@@ -11,6 +11,7 @@ function showRandomFighterJet() {
     let randomJet = fighterJets[randomIndex];
     document.getElementById("fighterJetName").textContent = randomJet.name;
     document.getElementById("fighterJetImage").src = randomJet.imageUrl;
+    document.getElementById("fighterJetLink").href = randomJet.wikiUrl;
     document.getElementById("RandomJet").style.display = "block";
 }
 
