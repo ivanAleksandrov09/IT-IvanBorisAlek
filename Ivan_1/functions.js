752b2de528efc0be831c817f5a8b53e99b961705
Ivan Alexandrov <ivan.d.aleksandrov.2023@elsys-bg.org>
2024-05-20 11:08:03 +0300

    fixed a bug + minor redesigns + new image url
    -fixed a bug where the gradient color of the background didnt reach the entire home page
    -fixed a slight misalignment of the footer
    -gave every image a slight border, every h1-h4 element a bold weight and made the text inside of the footer white
    -fixed the url to the image of the motorboat

--- Ivan_1/functions.js
+++ Ivan_1/functions.js
@@ -4,7 +4,7 @@ let boats = ["rowboat", "sailboat", "motorboat", "canoe", "yacht"];
 let urls = [
     "https://www.whitehallrow.com/wp-content/uploads/2021/06/classic-whitehall-spirit-14-traditional-rowboat-with-fixed-seats-menu.jpg",
     "https://assets-global.website-files.com/5ce44e4e8d1c1fc85b84c18d/630df6c4753999b44f9867b0_are-small-sailboats-or-big-sailboats-faster%253F.jpeg",
-    "https://yuxianhua.com/jpg/powerboat-1.jpg",
+    "https://images.boats.com/resize/wp/2/files/2016/07/powerboat-1.jpg",
     "https://bendingbranches.com/cdn/shop/articles/BB_Solo_five2nine_ca_99cbe5b8-3bb4-4427-9911-059de0605a13.jpg?v=1632501395",
     "https://www.fraseryachts.com/uploads/image/SILVER_EDGE.jpg",
 ];
